2670ed03475eb0b7f67c0f3dd301b028eae46cf9
ubernaut <collin.schroeder@gmail.com>
2024-09-25 19:07:23 -0800

    added eternal dragon

---
+++ src/pages/eternaldragon/index.js
@@ -0,0 +1,11 @@
+export const meta = {
+  title: 'Eternal Dragon',
+  tags: 'threejs,webvr',  
+  slug: 'eternaldragon',
+}
+
+export const options = {
+  display: 'fullscreen',
+}
+
+export { init } from './snektwo.js'
